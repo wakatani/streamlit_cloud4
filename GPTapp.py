666a137ddc2cffa8303c9859d043a7b673659e3d
Akiyoshi Wakatani <wakatani@konan-u.ac.jp>
2025-06-04 23:21:48 +0900

    June

--- GPTapp.py
+++ GPTapp.py
@@ -128,6 +128,39 @@ if st.button('問題 (Quiz)'):
                  options=(b[0], b[1], b[2], b[3], "わかりません (no idea)"),
                  index=4,
   )
+  if aaa:
+    try:
+      quiz_response=st.session_state['quiz']
+      explanation=st.session_state['expl']
+  
+      prob=quiz_response["問題文"]
+      code="{0}".format(quiz_response["Pythonコード"])
+      b[0]="１：{0}".format(quiz_response["選択肢１"])
+      b[1]="２：{0}".format(quiz_response["選択肢２"])
+      b[2]="３：{0}".format(quiz_response["選択肢３"])
+      b[3]="４：{0}".format(quiz_response["選択肢４"])
+      ans ="答えは{0}です。(The answer is {0}.)".format(quiz_response["答え"])
+      expl="  [ {0} ]".format(explanation)
+  
+      st.write(ans)
+      st.write(expl)
+      counter=st.session_state['counter']
+      msg="-----------------------------------------------------{0}".format(counter)
+      st.write(msg)
+      msg=prob
+      st.write(msg)
+      msg=code
+      st.code(msg)
+      for i in range(4):
+        st.write(b[i])
+      msg="-----------------------------------------------------"
+      st.write(msg)
+      msg="◇◇◇ 次の問題は「問題」を押してください (click Quiz, again.)"
+      st.write(msg)
+
+    except:
+      st.write('まず「問題」を押してください (click Quiz, first.)')
+    
 
   
   
